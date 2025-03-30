29e759baa0ca19174fee7bfd664ab3f6cd17920c
Oskar18 <775334585@seznam.cz>
2025-03-30 21:58:13 +0200

    Update ukol_1.py

--- ukol_1.py
+++ ukol_1.py
@@ -7,10 +7,12 @@ class Car:
         self.price = price
 
     def describe(self):
-        print(f"""
-        Značka: {self.brand}, Rychlost: {self.speed}, Objem motoru: {self.motor}, Barva: {self.color}, Cena: {self.price} ")
+        print(f" Značka: {self.brand}, Rychlost: {self.speed}, Objem motoru: {self.motor}, Barva: {self.color}, Cena: {self.price} ")
 
 ford = Car(brend: "ford", speed: "201", motor: "1500", color: "red", price: "700000")
 skoda = Car(brend: "skoda", speed: "200", motor: "1000", color: "blue", price: "800000")
-fiat = Car(brend: "fiat", speed: "190", motor: "1800", color: "white", price: "600000")""")
+fiat = Car(brend: "fiat", speed: "190", motor: "1800", color: "white", price: "600000")
 
+ford.describe()
+skoda.describe()
+fiat.describe()
